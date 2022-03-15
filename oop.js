a3f9733b22b73685e2d826e52233e0c0a4d8127b
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-03-15 01:30:24 -0700

    ES6 Classes

--- oop.js
+++ oop.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -62,3 +62,38 @@ const mercedes = new Car('Mercedes', 95);
 console.log(mercedes);
 mercedes.accelerate();
 mercedes.brake();
+*/
+
+// Class Expressions
+// const PersonCl = class {};
+
+// Class Declarations
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey, ${this.firstName}!`);
+  }
+}
+
+const Jessica = new PersonCl('Jessica', 1996);
+console.log(Jessica);
+
+Jessica.calcAge();
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}!`);
+// };
+Jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
