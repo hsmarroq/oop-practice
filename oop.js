251317dd9f900ba7639fc67d5cfe55e8bd993a69
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-03-15 01:13:48 -0700

    Coding Challenge #1

--- oop.js
+++ oop.js
@@ -35,3 +35,30 @@ matilda.calcAge();
 
 Person.prototype.species = 'Homo Sapiens';
 console.log(jonas.species, matilda.species);
+
+console.log(jonas.__proto__.__proto__);
+
+// Coding Challenge #1 //
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h.`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h.`);
+};
+
+const bmw = new Car('BMW', 120);
+console.log(bmw);
+bmw.accelerate();
+bmw.brake();
+const mercedes = new Car('Mercedes', 95);
+console.log(mercedes);
+mercedes.accelerate();
+mercedes.brake();
