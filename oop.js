0a1890bfacb289eae8b633260f2f0e35afb2b0d9
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-03-15 23:56:33 -0700

    Chaining Methods

--- oop.js
+++ oop.js
@@ -358,16 +358,19 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Your loan has been approved.`);
+      return this;
     }
   }
 
@@ -395,3 +398,4 @@ Account.helper();
 
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
