09b2e4a7c072f49791d27d609b30e1e74ea61ced
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-03-15 23:20:59 -0700

    Encapsulation: Protected Properties and Methods.

--- oop.js
+++ oop.js
@@ -331,28 +331,33 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // Protected Property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an accounr, ${this.owner}!`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan() {
+  _approveLoan() {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Your loan has been approved.`);
     }
@@ -365,6 +370,7 @@ acc1.deposit(150);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
